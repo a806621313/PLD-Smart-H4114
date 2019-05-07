fc13f5cb7dcbcd11658b8d0fb37cc96d5a331909
SHU <a806621313@gmail.com>
2019-05-07 09:12:26 +0200

    add dbscan

--- H4114/src/main/webapp/js/setMap.js
+++ H4114/src/main/webapp/js/setMap.js
@@ -1,104 +1,3 @@
-/**
-var map = L.map('map').fitWorld();
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-
-function createMarker(colorNumber){
-    var marker = L.AwesomeMarkers.icon({
-    prefix: "fa",
-    icon: 'star',
-    markerColor: NumberToIcon(colorNumber)
-    });
-    return marker;  
-}
-
-var positions = [];
-function createPositions(){
-    positions.push({x: rnd(d.x, max_x_stdev), y: rnd(d.y, max_y_stdev)});
-    
-}
-
-function onLocationFound(e) {
-    //var radius = e.accuracy / 2;
-    
-
-    L.marker(e.latlng,{icon: createMarker(1)}).addTo(map);
-	//L.marker(e.latlng-[0.00001,0.00001]).addTo(map);
-	//L.marker([45.781764, 4.872399],{color: 'red'}).addTo(map);
-        //.bindPopup("You are within " + radius + " meters from this point").openPopup();
-	
-    //L.circle(e.latlng, radius).addTo(map);
-	//dbscan();
-	
-}
-
-class cluster_position{
-    constructor(lat,lng,cluster){
-        this.latlng = L.latLng(lat, lng);
-        this.cluster = cluster;        
-    }
-    
-    
-    
-}
-
-function dbscan() {
-    
-    function generate_cluster_data() {
-		var num_clusters = 3;
-		var max_x_stdev = 10;
-		var max_y_stdev = 15;
-		var cluster_size = 30;
-
-		var raw_point_data = [];
-		var cluster_centers = [];
-		for (var i = 0; i < num_clusters; i++) {
-			cluster_centers.push({x: Math.random() * (width - 30), y: Math.random() * (height - 30)});
-		}
-
-		cluster_centers.forEach(function (d) {
-			for (var i = 0; i < cluster_size; i++) {
-				raw_point_data.push({x: rnd(d.x, max_x_stdev), y: rnd(d.y, max_y_stdev)});
-			}
-		});
-
-		return raw_point_data;
-	}
-    
-    var raw_point_data = generate_cluster_data();
-    
-    var dbscanner = jDBSCAN().eps(30).minPts(1).distance('EUCLIDEAN').data(positions);
-    var point_assignment_result = dbscanner();
-    console.log('Resulting DBSCAN output', point_assignment_result);
-
-    
-}
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-map.on('click', onMapClick);
-
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
-
-map.locate({setView: true, maxZoom: 16});
-*/
-
 var positions = [];
 function createPositions(){
     positions.push({location: {latitude: 45.782019, longitude: 4.872554}});
@@ -158,14 +57,8 @@ function initMap() {
             map: map,
             icon: { url: "./icons/man-pink-user.png"}
             });
-        }
-        
-        
+        }   
     }
-    
-    
-    
-    console.log('initial map');
 }
 
 var x = document.getElementById("demo");
@@ -180,7 +73,7 @@ function getLocation() {
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 }
-
+/**
 function NumberToIcon(number){
     switch(number){
         default:
@@ -203,7 +96,7 @@ class cluster_position{
         this.latlng = latlng;
         this.cluster = cluster;        
     }
-}
+}*/
 
 function dbscan() {
     //createPositions();
